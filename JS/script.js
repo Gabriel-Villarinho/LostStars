42198c42b8f03b2386dd34de7d5b707d08c5e8a4
Gabriel-Villarinho <gabriel.lima.villarinho@gmail.com>
2024-04-02 19:39:03 -0300

    continuando a programação

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,5 @@
 const alice = document.querySelector('.alice');
+const espinho = document.querySelector('.espinho')
 
 const pulo = () => {
     alice.classList.add('pulo');
@@ -10,5 +11,29 @@ const pulo = () => {
     }, 500);
 
 }
+
+const loop = setInterval(() => {
+
+    const espinhoPosition = espinho.offsetLeft
+    const alicePosition = +window.getComputedStyle(alice).bottom.replace('px', '')
+
+    //console.log(alicePosition)
+
+    //console.log(espinhoPosition)
+
+    if(espinhoPosition <= 120 && espinhoPosition > 0 && alicePosition < 80){
+        espinho.style.animation = 'none'
+        espinho.style.left = `${espinhoPosition}px`
+
+        alice.style.animation = 'none'
+        alice.style.bottom = `${alicePosition}px`
+
+        alice.src = 'CSS/IMG/morte.jpg'
+        alice.style.width = '75px'
+        alice.style.marginLeft = '50px'
+
+        clearInterval(loop)
+    }
+}, 10)
 
 document.addEventListener('keydown', pulo);
