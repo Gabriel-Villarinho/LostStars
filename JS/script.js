30a7c7009283cabc56ae244e1117f2a2258e79db
Gabriel-Villarinho <gabriel.lima.villarinho@gmail.com>
2024-04-09 13:39:31 -0300

    terminando os botoes

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,19 @@
 const alice = document.querySelector('.alice');
 const espinho = document.querySelector('.espinho')
 const pontos = document.querySelector('.pontos')
+const planeta = document.querySelector('.planeta');
+const jogoBase = document.querySelector('.jogo-base');
+const btn = document.querySelector('.start-button')
+const restartbtn = document.querySelector('.restart-button')
+const url = './index.html'
+const url2 = './fimjogo.html'
+
 let contar = 0
+
+function abrirOutraAba(url){
+  const win = window.open(url, '_blank')
+  win.focus
+}
 
 const pulo = () => {
     alice.classList.add('pulo');
@@ -30,10 +42,12 @@ const loop = setInterval(() => {
         alice.style.animation = 'none'
         alice.style.bottom = `${alicePosition}px`
 
-        alice.src = 'CSS/IMG/morte.jpg'
+        alice.src = 'CSS/IMG/morte.gif'
         alice.style.width = '75px'
         alice.style.marginLeft = '50px'
 
+        // acha um jeito ai de fazer q quando a alice morrer abrir o fimjogo.html
+        abrirOutraAba(url2)
 
 
         clearInterval(loop)
@@ -46,3 +60,33 @@ const loop = setInterval(() => {
 
 
 document.addEventListener('keydown', pulo);
+
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+      document.documentElement.classList.add('change-bg');
+    }, 5000);
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+        planeta.style.opacity = 0;
+        setTimeout(function() {
+          planeta.src = 'CSS/IMG/planeta2.png'; // 
+          planeta.style.opacity = 1; 
+        }, 1000); 
+      }, 5000); 
+    });
+
+    document.addEventListener('DOMContentLoaded', function() {
+        setTimeout(function() {
+          jogoBase.classList.add('ativo');
+        }, 5000);
+      });
+
+btn.addEventListener('click', () => {
+  abrirOutraAba(url)
+})
+
+restartbtn.addEventListener('click', () => {
+  abrirOutraAba(url)
+})
